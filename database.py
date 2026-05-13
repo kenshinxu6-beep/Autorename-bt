7cf0ee1e426aa8d22b9d490eb2a97f657da5b142
kenshinxu6-beep <kenshinxu.6@gmail.com>
2026-05-13 15:10:54 +0530

    Update database.py

--- database.py
+++ database.py
@@ -11,16 +11,10 @@ settings = db["settings"]
 
 async def init_db():
     await users.create_index("user_id", unique=True)
-    await settings.update_one(
-        {"_id": "global"},
-        {"$setOnInsert": {
-            "max_concurrent_normal": 10,
-            "max_concurrent_admin": 100
-        }},
-        upsert=True
-    )
+    await settings.update_one({"_id": "global"}, {"$setOnInsert": {
+        "max_concurrent_normal": 10, "max_concurrent_admin": 100
+    }}, upsert=True)
 
-# ---------- User Status Checks ----------
 async def is_admin(user_id):
     user = await users.find_one({"user_id": user_id})
     return user and user.get("is_admin", False)
@@ -29,16 +23,12 @@ async def is_premium(user_id):
     user = await users.find_one({"user_id": user_id})
     return user and user.get("is_premium", False)
 
-# ---------- Global Settings ----------
 async def get_settings():
-    s = await settings.find_one({"_id": "global"})
-    return s if s else {"max_concurrent_normal": 10, "max_concurrent_admin": 100}
+    return await settings.find_one({"_id": "global"}) or {"max_concurrent_normal": 10, "max_concurrent_admin": 100}
 
 async def get_global_setting(key, default=None):
     doc = await settings.find_one({"_id": "global"})
-    if doc and key in doc:
-        return doc[key]
-    return default
+    return doc.get(key, default) if doc else default
 
 async def set_global_setting(key, value):
     await settings.update_one({"_id": "global"}, {"$set": {key: value}}, upsert=True)
@@ -46,12 +36,9 @@ async def set_global_setting(key, value):
 async def delete_global_setting(key):
     await settings.update_one({"_id": "global"}, {"$unset": {key: ""}})
 
-# ---------- Per-User Settings ----------
 async def get_user_setting(user_id, key, default=None):
     user = await users.find_one({"user_id": user_id})
-    if user and key in user:
-        return user[key]
-    return default
+    return user.get(key, default) if user else default
 
 async def set_user_setting(user_id, key, value):
     await users.update_one({"user_id": user_id}, {"$set": {key: value}}, upsert=True)
@@ -59,46 +46,16 @@ async def set_user_setting(user_id, key, value):
 async def delete_user_setting(user_id, key):
     await users.update_one({"user_id": user_id}, {"$unset": {key: ""}})
 
-# ---------- Admin Management ----------
-async def add_admin(user_id):
-    await users.update_one({"user_id": user_id}, {"$set": {"is_admin": True}}, upsert=True)
+async def add_admin(uid): await set_user_setting(uid, "is_admin", True)
+async def remove_admin(uid): await delete_user_setting(uid, "is_admin")
+async def add_premium(uid): await set_user_setting(uid, "is_premium", True)
+async def remove_premium(uid): await delete_user_setting(uid, "is_premium")
 
-async def remove_admin(user_id):
-    await users.update_one({"user_id": user_id}, {"$unset": {"is_admin": ""}})
-
-# ---------- Premium Management ----------
-async def add_premium(user_id):
-    await users.update_one({"user_id": user_id}, {"$set": {"is_premium": True}}, upsert=True)
-
-async def remove_premium(user_id):
-    await users.update_one({"user_id": user_id}, {"$unset": {"is_premium": ""}})
-
-# ---------- Token Management ----------
-async def add_bot_token(token, dump_channel_id):
-    await tokens.update_one(
-        {"token": token},
-        {"$set": {"dump_channel": dump_channel_id}},
-        upsert=True
-    )
-
-async def remove_bot_token(token):
-    await tokens.delete_one({"token": token})
-
-async def get_all_tokens():
-    cursor = tokens.find({})
-    return await cursor.to_list(length=1000)
-
-# ---------- Dump Channel Management ----------
-async def add_dump_channel(channel_id, token):
-    await dumps.update_one(
-        {"channel_id": channel_id},
-        {"$set": {"token": token}},
-        upsert=True
-    )
-
-async def remove_dump_channel(channel_id):
-    await dumps.delete_one({"channel_id": channel_id})
-
-async def get_all_dumps():
-    cursor = dumps.find({})
-    return await cursor.to_list(length=1000)
+async def add_bot_token(token, dump_id):
+    await tokens.update_one({"token": token}, {"$set": {"dump_channel": dump_id}}, upsert=True)
+async def remove_bot_token(token): await tokens.delete_one({"token": token})
+async def get_all_tokens(): return await tokens.find({}).to_list(length=1000)
+async def add_dump_channel(ch_id, token):
+    await dumps.update_one({"channel_id": ch_id}, {"$set": {"token": token}}, upsert=True)
+async def remove_dump_channel(ch_id): await dumps.delete_one({"channel_id": ch_id})
+async def get_all_dumps(): return await dumps.find({}).to_list(length=1000)
